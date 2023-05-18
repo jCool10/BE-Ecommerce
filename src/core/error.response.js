2c2c528d8f1af94c699696df14a0122dfb6fd057
jCool10 <valentinohoang1908@gmail.com>
2023-05-18 20:36:08 +0700

    update

--- src/core/error.response.js
+++ src/core/error.response.js
@@ -1,64 +1,39 @@
-const { ReasonPhrases, StatusCodes } = require('../utils/httpStatusCode')
-
-const StatusCode = {
-  FORBIDDEN: 403,
-  CONFLICT: 409,
-}
-
-const ReasonStatusCode = {
-  FORBIDDEN: 'Bad request error',
-  CONFLICT: 'Conflict Error',
-}
+const { ReasonPhrases, StatusCodes } = require("../utils/httpStatusCode");
 
 class ErrorResponse extends Error {
   constructor(message, status) {
-    super(message)
-    this.status = status
+    super(message);
+    this.status = status;
   }
 }
 
 class ConflictRequestError extends ErrorResponse {
-  constructor(
-    message = ReasonStatusCode.CONFLICT,
-    statusCode = StatusCode.FORBIDDEN
-  ) {
-    super(message, statusCode)
+  constructor(message = ReasonPhrases.CONFLICT, statusCode = StatusCodes.CONFLICT) {
+    super(message, statusCode);
   }
 }
 
 class BadRequestError extends ErrorResponse {
-  constructor(
-    message = ReasonStatusCode.CONFLICT,
-    statusCode = StatusCode.FORBIDDEN
-  ) {
-    super(message, statusCode)
+  constructor(message = ReasonPhrases.BAD_REQUEST, statusCode = StatusCodes.BAD_REQUEST) {
+    super(message, statusCode);
   }
 }
 
 class AuthFailureError extends ErrorResponse {
-  constructor(
-    message = ReasonPhrases.UNAUTHORIZED,
-    status = StatusCodes.UNAUTHORIZED
-  ) {
-    super(message, status)
+  constructor(message = ReasonPhrases.UNAUTHORIZED, status = StatusCodes.UNAUTHORIZED) {
+    super(message, status);
   }
 }
 
 class NotFoundError extends ErrorResponse {
-  constructor(
-    message = ReasonPhrases.NOT_FOUND,
-    status = StatusCodes.NOT_FOUND
-  ) {
-    super(message, status)
+  constructor(message = ReasonPhrases.NOT_FOUND, status = StatusCodes.NOT_FOUND) {
+    super(message, status);
   }
 }
 
 class ForbiddenError extends ErrorResponse {
-  constructor(
-    message = ReasonPhrases.FORBIDDEN,
-    status = StatusCodes.FORBIDDEN
-  ) {
-    super(message, status)
+  constructor(message = ReasonPhrases.FORBIDDEN, status = StatusCodes.FORBIDDEN) {
+    super(message, status);
   }
 }
 
@@ -68,4 +43,4 @@ module.exports = {
   AuthFailureError,
   NotFoundError,
   ForbiddenError,
-}
+};
